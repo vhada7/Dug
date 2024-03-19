99549f8257159666cca39f78984d4bec9774dabf
vhada7 <hadav@rpi.edu>
2024-03-18 20:27:29 -0400

    Added Graph Components
    Filled with sample data for now.

--- dug/src/Pages/Home.js
+++ dug/src/Pages/Home.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import './Home.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Sidebar from '../Components/Sidebar'
-
 
 
 
 const Home = () => {
     return ( 
         <div id="home" className='content content-center'>
-            <Sidebar />
             <div className="home-content">
                 <h1>Home</h1>
                 <p>Home page body content</p>
+        </div>
         </div>
     );
 }
